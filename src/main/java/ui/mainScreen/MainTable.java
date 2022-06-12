1affa48eba5d29b4b54393eee487ed11650e6896
VassBassApp <vassbassapp@gmail.com>
2022-06-12 13:53:00 +0300

    Working...please wait

--- src/main/java/ui/mainScreen/MainTable.java
+++ src/main/java/ui/mainScreen/MainTable.java
@@ -10,8 +10,44 @@ public class MainTable extends JTable {
     private static final String EQUATION = "Equation";
     private static final String RESULT = "Result";
 
+    private ArrayList<Equation>list;
+
     public MainTable(ArrayList<Equation>list){
         super(tableModel(list));
+        this.list = list;
+    }
+
+    public void addEquation(Equation equation){
+        if (equation != null) {
+            list.add(equation);
+            setList(list);
+        }
+    }
+
+    public void removeEquation(int id){
+        if (id >= 0) {
+            for (int index = 0; index < list.size(); index++) {
+                Equation e = list.get(index);
+                if (e.getId() == id) {
+                    list.remove(index);
+                    break;
+                }
+            }
+            setList(list);
+        }
+    }
+
+    public void setEquation(Equation oldEquation, Equation newEquation){
+
+    }
+
+    public void setList(ArrayList<Equation>list){
+        if (list != null) this.setModel(tableModel(list));
+    }
+
+    public Equation getSelectedEquation(){
+        int selected = this.getSelectedRow();
+        return selected >= 0 && selected < list.size() ? list.get(selected) : null;
     }
 
     private static DefaultTableModel tableModel(ArrayList<Equation>list){
