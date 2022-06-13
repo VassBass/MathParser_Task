20468a3a2d70c074def6976e7e9e3902181f33d4
VassBassApp <vassbassapp@gmail.com>
2022-06-13 21:31:06 +0300

    Working...please wait

--- src/main/java/repository/EquationRepository_impl.java
+++ src/main/java/repository/EquationRepository_impl.java
@@ -1,13 +1,44 @@
 package repository;
 
 import model.Equation;
+import org.sqlite.JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class EquationRepository_impl implements EquationRepository {
+    private static String DB_URL = "jdbc:sqlite:Data.db";
+
+    private Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new JDBC());
+        return DriverManager.getConnection(DB_URL);
+    }
 
     public EquationRepository_impl(){
+        createTableIfNotExists();
+    }
 
+    public EquationRepository_impl(String dbUrl){
+        DB_URL = dbUrl;
+        createTableIfNotExists();
+    }
+
+    private void createTableIfNotExists(){
+        String sql = "CREATE TABLE IF NOT EXISTS equations ("
+                + "id integer NOT NULL UNIQUE"
+                + ", equation text NOT NULL"
+                + ", result real NOT NULL"
+                + ", PRIMARY KEY (\"id\")"
+                + ");";
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()){
+            statement.execute(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
